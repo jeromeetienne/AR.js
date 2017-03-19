408cda8381648d3696a45d16e9456cf4b37324d3
Jerome Etienne <jerome.etienne@gmail.com>
2017-03-19 12:15:45 +0000

    added opacity animation in marker cloak

--- three.js/threex-armarkercloak.js
+++ three.js/threex-armarkercloak.js
@@ -15,10 +15,14 @@ THREEx.ArMarkerCloak = function(videoTexture){
 	var material = new THREE.ShaderMaterial( {
 		vertexShader: THREEx.ArMarkerCloak.vertexShader,
 		fragmentShader: THREEx.ArMarkerCloak.fragmentShader,
+                transparent: true,
 		uniforms: {
 			texture: {
 				value: videoTexture
 			},
+                        opacity: {
+                                value: 0.5
+                        }
 		},
 		defines: {
 			updateInShaderEnabled: updateInShaderEnabled ? 1 : 0,
@@ -209,9 +213,10 @@ THREEx.ArMarkerCloak.vertexShader = THREEx.ArMarkerCloak.markerSpaceShaderFuncti
 THREEx.ArMarkerCloak.fragmentShader = '\n'+
 '	varying vec2 vUv;\n'+
 '	uniform sampler2D texture;\n'+
+'	uniform float opacity;\n'+
 '\n'+
 '	void main(void){\n'+
 '		vec3 color = texture2D( texture, vUv ).rgb;\n'+
 '\n'+
-'		gl_FragColor = vec4( color, 1.0);\n'+
+'		gl_FragColor = vec4( color, opacity);\n'+
 '	}'
