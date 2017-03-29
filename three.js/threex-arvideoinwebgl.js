25c53ea882def094f3af871d934dc71c834dcdf1
Jerome Etienne <jerome.etienne@gmail.com>
2017-03-29 15:28:47 +0100

    more on video in webgl

--- three.js/threex-arvideoinwebgl.js
+++ three.js/threex-arvideoinwebgl.js
@@ -3,7 +3,6 @@ var THREEx = THREEx || {}
 THREEx.ArVideoInWebgl = function(videoTexture){	
 	var _this = this
 	
-	this.stereo = false
 	//////////////////////////////////////////////////////////////////////////////
 	//	plane always in front of the camera, exactly as big as the viewport
 	//////////////////////////////////////////////////////////////////////////////
@@ -39,11 +38,6 @@ THREEx.ArVideoInWebgl = function(videoTexture){
 		
 		// extract the fov from the projectionMatrix
 		var fov = THREE.Math.radToDeg(Math.atan(1/camera.projectionMatrix.elements[5])) *2;
-		
-		// TODO accessible global yuck
-		if( _this.stereo === true ){
-			fov *= 2
-		}
 		
 		var aspect = parseFloat(arToolkitSource.domElement.style.width.replace(/px$/,''))
 			/
