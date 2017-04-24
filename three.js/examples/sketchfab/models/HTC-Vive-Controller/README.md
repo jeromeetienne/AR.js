178baa5994f0cf5080d3a59e6ebcc9a07d4ae2ce
Jerome Etienne <jerome.etienne@gmail.com>
2017-04-24 16:44:28 +0200

    more work

---
+++ three.js/examples/sketchfab/models/HTC-Vive-Controller/README.md
@@ -0,0 +1,1 @@
+from https://sketchfab.com/models/7199d01cef484c7ba67e0cf8ca690d97#
--- three.js/examples/sketchfab/src/bookmarklet.js
+++ three.js/examples/sketchfab/src/bookmarklet.js
@@ -5,7 +5,6 @@ var container = document.querySelector('.general-controls.widget')
 
 
 var domElement = document.createElement('a')
-domElement.href = 'http://googgle.com'
 domElement.href = 'javascript:void(0)'
 domElement.innerHTML = 'AR.js'
 // container.appendChild(domElement)
@@ -26,7 +25,21 @@ container.insertBefore(domElement, container.firstChild);
 
 
 domElement.addEventListener('click', function(){
-	alert('jerome etienne, c\'est mieux que james bond')	
+	// alert('jerome etienne, c\'est mieux que james bond')	
+	// TODO add an image AR-Code inside the player
+	// three.js/examples/sketchfab/images/ar-code.png
+
+
+	var domElement = document.createElement('img')
+	domElement.src = 'http://127.0.0.1:8080/three.js/examples/sketchfab/images/ar-code.png'
+	domElement.style.width = 'auto'
+	domElement.style.height = 'auto'
+	domElement.style.maxWidth = '80%'
+	domElement.style.maxHeight = '80%'
+	
+
+	var container = document.querySelector('.gui.enabled')
+	container.appendChild(domElement)
 })
 
 })()
