e22b7b368b2e6146dc9bb2e465490c6977be7f03
Jerome Etienne <jerome.etienne@gmail.com>
2017-05-19 12:47:42 +0100

    added shaddow in minecraft demo scenes - dynamic switch

--- three.js/examples/demo-scenes/threex-demoscenes.js
+++ three.js/examples/demo-scenes/threex-demoscenes.js
@@ -17,6 +17,10 @@ THREEx.DemoContent.prototype.update = function (delta) {
 	})
 }
 
+THREEx.DemoContent.prototype.dispose = function (delta) {
+	this._onRenderFcts = []
+}
+
 THREEx.DemoContent.prototype.createMarkerScene = function (sceneName) {
 
 	if( sceneName === 'torus' ){
@@ -133,9 +137,49 @@ THREEx.DemoContent.prototype._createMinecraft = function () {
 	bodyAnims.start('hiwave');
 	this._onRenderFcts.push(function(delta){
 		bodyAnims.update(delta)	
-	})		
+	})	
+	
+	var shadowMesh = createShadowMesh()
+	character.root.add(shadowMesh)
 
 	return markerScene
+	
+	function createShadowMesh(){
+		// create texture
+		var canvas = document.createElement( 'canvas' );
+		canvas.width = 128;
+		canvas.height = 128;
+		var texture = new THREE.Texture( canvas );
+		texture.needsUpdate = true
+		// draw on texture
+		var context = canvas.getContext( '2d' );
+		var gradient = context.createRadialGradient(
+			canvas.width / 2,
+			canvas.height / 2,
+			0,
+			canvas.width / 2,
+			canvas.height / 2,
+			canvas.width / 2
+		);
+		gradient.addColorStop( 0.2, 'rgba(30,30,30,1)' );
+		gradient.addColorStop( 0.4, 'rgba(30,30,30,0.8)' );
+		gradient.addColorStop( 0.6, 'rgba(30,30,30,0.5)' );
+		gradient.addColorStop( 1, 'rgba(30,30,30,0)' );
+		context.fillStyle = gradient;
+		context.fillRect( 0, 0, canvas.width, canvas.height );
+
+		// build mesh
+		var geometry = new THREE.PlaneBufferGeometry( 1, 1, 3, 3 );
+		geometry.scale(0.8,0.8,0.8)
+		var material = new THREE.MeshBasicMaterial({
+			map: texture,
+			transparent: true,
+		});
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.rotation.x = - Math.PI / 2;
+
+		return mesh
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////
