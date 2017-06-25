f98bfd6fc81de0f9f6b9abb21a4884cd18c44723
Jerome Etienne <jerome.etienne@gmail.com>
2017-06-25 19:44:42 +0100

    cleanup

--- three.js/examples/augmented-webpages/js/firebase.js
+++ three.js/examples/augmented-webpages/js/firebase.js
@@ -12,10 +12,10 @@ function firebaseInit(){
 		// storageBucket: "augmented-webpages.appspot.com",
 		// messagingSenderId: "128557805583"
 	};
-	var firebaseApp = firebase.initializeApp(config);
+	firebase.initializeApp(config)
 
 
-	var dataRef = firebase.database().ref('marker-page-'+firebasePeerID);
+	var dataRef = firebase.database().ref('marker-page-'+firebasePeerID)
 	dataRef.set({
 		qrCodeToScan: true,
 		createdAt : new Date().toJSON()
@@ -29,17 +29,34 @@ function firebaseInit(){
 				qrCodeToScan : true
 			})
 		}
-	});
+	})
+	
+	// remove all records older than 30min
+	purgeObsoleteRecords(30*60)
 
 	return
+	
+	//////////////////////////////////////////////////////////////////////////////
+	//		create GUID
+	//////////////////////////////////////////////////////////////////////////////
 	function createGUID() {
 		function s4() {
-			return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
+			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
 		}
-		// return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		// s4() + '-' + s4() + s4() + s4();
+		// return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
 		return s4() + s4()
+	}
+	
+	function purgeObsoleteRecords(maxAge){
+		var ref = firebase.database().ref()
+		ref.on("child_added", function(snapshot){
+			if( snapshot.key.match('marker-page-') === null )	return
+			var createdAtString = snapshot.child("createdAt").val()
+			var createdAt = new Date(createdAtString)
+			var ageSeconds = (Date.now() - createdAt.getTime()) / 1000
+			if( ageSeconds > maxAge ){
+				snapshot.ref.remove()
+			}
+		})
 	}
 }
