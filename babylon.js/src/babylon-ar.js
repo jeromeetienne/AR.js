9ad1b366182b112c8f7ea7dfa2c8bd5321ad8f3a
Jerome Etienne <jerome.etienne@gmail.com>
2017-08-11 11:34:03 +0100

    more work

--- babylon.js/src/babylon-ar.js
+++ babylon.js/src/babylon-ar.js
@@ -9,44 +9,47 @@ ARjs.Babylon.createCamera = function(scene){
 	// arcRotateCamera.setTarget(BABYLON.Vector3.Zero());
 	// arcRotateCamera.attachControl(canvas, true);
 
-	scene.activeCamera.fovmode = BABYLON.Camera.fovmode_HORIZONTAL_FIXED;
-	scene.activeCamera.fov = 2*22 / 180*Math.PI
+	// hard
+	// scene.activeCamera.fovmode = BABYLON.Camera.fovmode_HORIZONTAL_FIXED;
+	// scene.activeCamera.fov = 2*22 / 180*Math.PI
 	return babylonCamera
 }
 
 ARjs.Babylon.updateCamera = function(babylonCamera, threeCamera){
-	
+	var projectionMatrixArr = threeCamera.projectionMatrix.toArray()
+	var babylonMatrix = BABYLON.Matrix.FromArray(projectionMatrixArr)
+	babylonCamera.freezeProjectionMatrix(babylonMatrix)
 }
 
 ARjs.Babylon.updateObjectPose = function(babylonObject3D, threeObject3D){
-	var modelViewMatrix = threeObject3D.matrix
 
 	threeObject3D.updateMatrixWorld()
-// console.log('camera position', threeObject3D.position)
 
-	var threejsPosition = threeObject3D.position.clone()
+	// console.log('camera position', threeObject3D.position)
 
-	var threejsTarget = new THREE.Vector3(0,0,-1)
-	threeObject3D.localToWorld(threejsTarget)
+	// var threejsPosition = threeObject3D.position.clone()
+	// babylonObject3D.position.x =  threejsPosition.x
+	// babylonObject3D.position.y =  threejsPosition.y
+	// babylonObject3D.position.z =  threejsPosition.z
 
+	// var threejsUp = new THREE.Vector3(0,1,0)
+	// // threeObject3D.localToWorld(threejsUp)
+	// babylonObject3D.upVector.x = threejsUp.x
+	// babylonObject3D.upVector.y = threejsUp.y
+	// babylonObject3D.upVector.z = threejsUp.z
 
-	babylonObject3D.position.x =  threejsPosition.x
-	babylonObject3D.position.y =  threejsPosition.y
-	babylonObject3D.position.z =  threejsPosition.z
+	// // set target
+	// var threejsTarget = new THREE.Vector3(0,0,-1)
+	// threeObject3D.localToWorld(threejsTarget)
+	// babylonObject3D.setTarget(new BABYLON.Vector3(
+	// 	threejsTarget.x,
+	// 	threejsTarget.y,
+	// 	threejsTarget.z
+	// ))
 
-	babylonObject3D.setTarget(new BABYLON.Vector3(
-		threejsTarget.x,
-		threejsTarget.y,
-		threejsTarget.z
-	))
-
-		// cameraBabylon.target.x = -threejsTarget.x
-		// cameraBabylon.target.y = 0
-		// cameraBabylon.target.z = 0
-		// cameraBabylon.setPosition()
-// debugger
-		// cameraBabylon._computedViewMatrix = new BABYLON.Matrix.FromArray(modelViewMatrix.toArray());
-		// cameraBabylon._computedViewMatrix.m.set(modelViewMatrix.toArray());
-		// cameraBabylon._computedViewMatrix.invert()
-	
+	// use modelViewMatrix
+	var modelViewMatrix = threeObject3D.matrix
+	babylonObject3D._computedViewMatrix = new BABYLON.Matrix.FromArray(modelViewMatrix.toArray());
+	babylonObject3D._computedViewMatrix.m.set(modelViewMatrix.toArray());
+	babylonObject3D._computedViewMatrix.invert()	
 }
