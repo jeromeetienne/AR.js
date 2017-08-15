bc40999b3c94aa818663fc288940ef58ba8dd7e1
Jerome Etienne <jerome.etienne@gmail.com>
2017-08-15 19:32:56 +0100

    support for customination of ARjs.SessionDebugUI.AugmentedWebsiteURL

--- three.js/src/new-api/arjs-debugui.js
+++ three.js/src/new-api/arjs-debugui.js
@@ -39,13 +39,15 @@ ARjs.SessionDebugUI = function(arSession, tangoPointCloud){
 	//////////////////////////////////////////////////////////////////////////////
 	//		augmented-websites
 	//////////////////////////////////////////////////////////////////////////////
-	var domElement = document.createElement('a')
-	domElement.innerHTML = 'Share on augmented-websites'
-	domElement.style.display = 'block'
-	// domElement.setAttribute('target', '_blank')
-	domElement.href = 'https://webxr.io/augmented-website?'+location.href
-	domElement.href = 'http://127.0.0.1:8080/augmented-website?'+location.href
-	this.domElement.appendChild(domElement)				
+
+	if( ARjs.SessionDebugUI.AugmentedWebsiteURL ){
+		var domElement = document.createElement('a')
+		domElement.innerHTML = 'Share on augmented-websites'
+		domElement.style.display = 'block'
+		// domElement.setAttribute('target', '_blank')
+		domElement.href = ARjs.SessionDebugUI.AugmentedWebsiteURL + '?'+location.href
+		this.domElement.appendChild(domElement)						
+	}
 
 	//////////////////////////////////////////////////////////////////////////////
 	//		toggle-point-cloud
@@ -71,6 +73,12 @@ ARjs.SessionDebugUI = function(arSession, tangoPointCloud){
 		})
 	}
 }
+
+/**
+ * Url of augmented-website service - if === '' then dont include augmented-website link
+ * @type {String}
+ */
+ARjs.SessionDebugUI.AugmentedWebsiteURL = 'https://webxr.io/augmented-website'
 
 //////////////////////////////////////////////////////////////////////////////
 //		ARjs.AnchorDebugUI
