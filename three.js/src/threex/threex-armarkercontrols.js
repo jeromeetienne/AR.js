7e247677f563e7858388b00eb46c226c92874bb5
Andrey Radchishin <andreyrd1@gmail.com>
2018-10-16 12:00:52 -0700

    Implement marker tracking smoothing from @ideolio PR #263

--- three.js/src/threex/threex-armarkercontrols.js
+++ three.js/src/threex/threex-armarkercontrols.js
@@ -35,6 +35,11 @@ ARjs.MarkerControls = THREEx.ArMarkerControls = function(context, object3d, para
 	this.object3d.matrixAutoUpdate = false;
 	this.object3d.visible = false
 
+	this.debounceMatrices = []; // last DEBOUNCE_COUNT modelViewMatrix
+	this.DEBOUNCE_COUNT = 10; // average over this many
+	this.AVERAGE_MATRIX_TOLERANCE = .01; // max allowable distance from current matrix entry to average
+	this.AVERAGE_MATRIX_THRESHOLD = 5;
+
 	//////////////////////////////////////////////////////////////////////////////
 	//		setParameters
 	//////////////////////////////////////////////////////////////////////////////
@@ -124,7 +129,42 @@ ARjs.MarkerControls.prototype.updateWithModelViewMatrix = function(modelViewMatr
 
 	// change markerObject3D.matrix based on parameters.changeMatrixMode
 	if( this.parameters.changeMatrixMode === 'modelViewMatrix' ){
-		markerObject3D.matrix.copy(modelViewMatrix)
+		var sum,
+				i, j,
+	 			renderReqd = false,
+	 			averages, // average values for matrix over last DEBOUNCE_COUNT
+	 			exceedsAverageTolerance = 0;
+
+		this.debounceMatrices.push(modelViewMatrix.elements.slice()); // add latest
+
+		if (this.debounceMatrices.length < (this.DEBOUNCE_COUNT + 1)) {
+			markerObject3D.matrix.copy(modelViewMatrix); // not enough for average
+		} else {
+			this.debounceMatrices.shift(); // remove oldest entry
+			averages = [];
+
+			for (i in modelViewMatrix.elements) { // loop over entries in matrix
+				sum = 0;
+				for (j in this.debounceMatrices) { // calculate average for this entry
+					sum += this.debounceMatrices[j][i];
+				}
+				averages[i] = sum / this.DEBOUNCE_COUNT;
+				// check how many elements vary from the average by at least AVERAGE_MATRIX_TOLERANCE
+				if (Math.abs(averages[i] - modelViewMatrix.elements[i]) >= this.AVERAGE_MATRIX_TOLERANCE) {
+					exceedsAverageTolerance++;
+				}
+			}
+			
+			// if moving (i.e. at least AVERAGE_MATRIX_THRESHOLD entries are over AVERAGE_MATRIX_TOLERANCE)
+			if (exceedsAverageTolerance >= this.AVERAGE_MATRIX_THRESHOLD) {
+				// then update matrix values to average, otherwise, don't render to minimize jitter
+				for (i in modelViewMatrix.elements) {
+					modelViewMatrix.elements[i] = averages[i];
+				}
+				markerObject3D.matrix.copy(modelViewMatrix);
+				renderReqd = true; // render required in animation loop
+			}
+		}
 	}else if( this.parameters.changeMatrixMode === 'cameraTransformMatrix' ){
 		markerObject3D.matrix.getInverse( modelViewMatrix )
 	}else {
@@ -136,6 +176,8 @@ ARjs.MarkerControls.prototype.updateWithModelViewMatrix = function(modelViewMatr
 
 	// dispatchEvent
 	this.dispatchEvent( { type: 'markerFound' } );
+
+	return renderReqd;
 }
 
 //////////////////////////////////////////////////////////////////////////////
