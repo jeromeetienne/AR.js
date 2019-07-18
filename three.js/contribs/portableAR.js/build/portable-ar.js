605d94469b4e558cea96e80a36e385956f8dc0f9
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-07-18 11:33:42 +0200

    Last commit before release

--- three.js/contribs/portableAR.js/build/portable-ar.js
+++ three.js/contribs/portableAR.js/build/portable-ar.js
@@ -53806,7 +53806,7 @@ Object.assign( ARjs.Context.prototype, THREE.EventDispatcher.prototype );
 // ARjs.Context.baseURL = '../'
 // default to github page
 ARjs.Context.baseURL = 'https://jeromeetienne.github.io/AR.js/three.js/'
-ARjs.Context.REVISION = '1.7.4';
+ARjs.Context.REVISION = '1.7.5';
 
 /**
  * Create a default camera for this trackingBackend
