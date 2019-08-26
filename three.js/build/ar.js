788d27310cb89be906506a7abbdbfd593a9303ac
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-08-26 11:54:56 +0200

    Last commit before release

--- three.js/build/ar.js
+++ three.js/build/ar.js
@@ -5308,7 +5308,7 @@ Object.assign( ARjs.Context.prototype, THREE.EventDispatcher.prototype );
 // ARjs.Context.baseURL = '../'
 // default to github page
 ARjs.Context.baseURL = 'https://jeromeetienne.github.io/AR.js/three.js/'
-ARjs.Context.REVISION = '1.7.7';
+ARjs.Context.REVISION = '1.7.8';
 
 /**
  * Create a default camera for this trackingBackend
