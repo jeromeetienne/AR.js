0b5c08b909bac9a93d62606480731626190ede07
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-01-18 16:39:01 +0100

    now tracking and show something on desktop

--- three.js/src/threex/threex-armarkercontrols.js
+++ three.js/src/threex/threex-armarkercontrols.js
@@ -263,6 +263,13 @@ ARjs.MarkerControls.prototype._initArtoolkit = function () {
         var pw = arController.canvas.width;
         var ph = arController.canvas.height;
 
+        var context_process = arController.canvas.getContext('2d');
+
+        function process() {
+            var imageData = context_process.getImageData(0, 0, pw, ph);
+            worker.postMessage({ type: "process", imagedata: imageData }, [imageData.data.buffer]);
+        }
+
         // initialize the worker
         worker.postMessage({
             type: 'init',
@@ -272,12 +279,67 @@ ARjs.MarkerControls.prototype._initArtoolkit = function () {
             param: arController.cameraParam.src,
         });
 
+        function multiplyMatrices(a, b) {
+            var ae = a;
+            var be = b;
+            var te = new Float64Array(16);
+
+            var a11 = ae[0], a12 = ae[4], a13 = ae[8], a14 = ae[12];
+            var a21 = ae[1], a22 = ae[5], a23 = ae[9], a24 = ae[13];
+            var a31 = ae[2], a32 = ae[6], a33 = ae[10], a34 = ae[14];
+            var a41 = ae[3], a42 = ae[7], a43 = ae[11], a44 = ae[15];
+
+            var b11 = be[0], b12 = be[4], b13 = be[8], b14 = be[12];
+            var b21 = be[1], b22 = be[5], b23 = be[9], b24 = be[13];
+            var b31 = be[2], b32 = be[6], b33 = be[10], b34 = be[14];
+            var b41 = be[3], b42 = be[7], b43 = be[11], b44 = be[15];
+
+            te[0] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
+            te[4] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
+            te[8] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
+            te[12] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
+
+            te[1] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
+            te[5] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
+            te[9] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
+            te[13] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
+
+            te[2] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
+            te[6] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
+            te[10] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
+            te[14] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
+
+            te[3] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
+            te[7] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
+            te[11] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
+            te[15] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
+
+            return te;
+        }
+
         worker.onmessage = function (ev) {
-            if (ev.data.type === 'found') {
-                // an NFT marker has been found, update its matrix using event data
-                onMarkerFound(ev);
+            if (ev && ev.data && ev.data.type === 'found') {
+                var proj = JSON.parse(ev.data.proj);
+                var world = JSON.parse(ev.data.matrixGL_RH);
+
+                // ~nicolocarpignoli the mulyiply matrixes is needed?
+                var mat = multiplyMatrices(proj, world);
+
+                onMarkerFound({
+                    data: {
+                        type: artoolkit.NFT_MARKER,
+                        matrix: mat,
+                    }
+                });
             }
+
+            // ~nicolocarpignoli this means that this run as a loop. Maybe we can add a minimal setTimeout
+            // otherwise it stresses the CPU and battery of device
+            process();
         };
+
+        // start the loop
+        process();
     }
 
     function onMarkerFound(event) {
--- three.js/vendor/jsartoolkit5/js/artoolkit.worker.js
+++ three.js/vendor/jsartoolkit5/js/artoolkit.worker.js
@@ -8,29 +8,40 @@ if ('function' === typeof importScripts) {
                 load(msg);
                 return;
             }
+            case "process": {
+                next = msg.imagedata;
+                process();
+                return;
+            }
         }
     };
+
+    var next = null;
+
+    var ar = null;
+    var markerResult = null;
 
     function load(msg) {
         var path = '../../';
 
         var onLoad = function () {
-            var ar = new ARController(msg.pw, msg.ph, param);
+            ar = new ARController(msg.pw, msg.ph, param);
+            var cameraMatrix = ar.getCameraMatrix();
+
 
             // after the ARController is set up, we load the NFT Marker
             ar.loadNFTMarker(path + msg.marker, function (markerId) {
                 ar.trackNFTMarkerId(markerId);
-            }, function(err) {
+            }, function (err) {
                 console.log('Error in loading marker on Worker', err)
             });
 
             // ...and we listen for event when marker has been found from camera
             ar.addEventListener('getNFTMarker', function (ev) {
                 // let AR.js know that a NFT marker has been found, with its matrix for positioning
-                postMessage({
-                    type: "found",
-                    matrix: JSON.stringify(ev.data.matrix),
-                });
+                // markerResult = { type: "found", matrix: JSON.stringify(ev.data.matrix) };
+                markerResult = {type: "found", matrixGL_RH: JSON.stringify(ev.data.matrixGL_RH), proj: JSON.stringify(cameraMatrix)};
+
             });
         };
 
@@ -41,4 +52,23 @@ if ('function' === typeof importScripts) {
         // we cannot pass the entire ARController, so we re-create one inside the Worker, starting from camera_param
         var param = new ARCameraParam(path + msg.param, onLoad, onError);
     }
+
+    function process() {
+        markerResult = null;
+
+        if (ar) {
+            ar.process(next);
+        }
+
+        if (markerResult) {
+            postMessage(markerResult);
+        } else {
+            postMessage({
+                type: "not found",
+            });
+        }
+        next = null;
+    }
+
 }
+
